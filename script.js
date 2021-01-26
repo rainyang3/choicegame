f5c694158bfd70580d583fea89a8cc27efd62477
rainyang3 <77300717+rainyang3@users.noreply.github.com>
2021-01-27 04:06:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 
 var questions= [
     {
-        prompt: "What color "
+        prompt: "What is my name? \n (a) Rain\n (b) Bob ",
+
+        answer: "a"
     }
 ]
+}
+
+var score = 0;
+
+for (var a=0; a< questions.length; a++){
+    var game = window.prompt(questions[a].prompt);
+    if(game == questions[a].answer){
+        score++;
+        alert("NICE!");
+
+    } else {
+        alert("WRONG!");
+    }
 }
